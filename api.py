8457c078ccb35969dc1944773a94c189e47e15b2
Brandon Wiley <brandon@blanu.net>
2015-10-11 10:28:23 -0500

    Added UserService

--- api.py
+++ api.py
@@ -8,3 +8,18 @@ from google.appengine.ext import db
 
 from rpc import JsonRpcService
 from models import *
+
+class UserService(JsonRpcService):
+  def json_isLoggedIn(self):
+    user = users.get_current_user()
+    logging.info('isLoggedIn '+str(user));
+    return user!=None
+
+  def json_isAdmin(self):
+    return users.is_current_user_admin()
+
+  def json_login(self):
+    return users.create_login_url('/admin')
+
+  def json_logout(self):
+    return users.create_logout_url('/')
